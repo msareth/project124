592b2e1b5967c6967c99ffabc9c4efb84c6d86f3
msareth <102205667+msareth@users.noreply.github.com>
2022-06-14 22:10:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+no1 = document.getElementById('input1').value
+no2 = document.getElementById('input2').value
+
+function calculate() {
+  result = no1 + no2
+  document.getElementById('results').innerHTML = result
+}
